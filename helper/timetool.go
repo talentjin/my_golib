3e1ed3b00eb55fe21ef68db0828380c4ccf85046
sunjin <254444589@qq.com>
2018-06-29 11:58:02 +0800

    测试

--- helper/timetool.go
+++ helper/timetool.go
@@ -2,14 +2,14 @@ package helper
 
 import "time"
 
-type timeTool struct{}
+type TimeTool struct{}
 
-func (*timeTool) MakeTimeToDate(timestamp int64) string {
+func (*TimeTool) MakeTimeToDate(timestamp int64) string {
 	dateStr := time.Unix(timestamp, 0).Format("20060102")
 	return dateStr
 }
 
-func (*timeTool) MakeDateToTime(dateStr string) int64 {
+func (*TimeTool) MakeDateToTime(dateStr string) int64 {
 	t, _ := time.Parse("20060102", dateStr)
 	timeStamp := t.Unix()
 	return timeStamp
